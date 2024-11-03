a8cf4fe8f792493d1cb1f6d61fc2105728307dc7
bellobambo <bellobambo21@gmail.com>
2024-11-03 12:14:52 +0100

    make transactions

--- test.sol
+++ test.sol
@@ -1,15 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-
-contract MyContract{
+contract MyContract {
+    address public lastSender;
 
     // Recive Payment
-    function receive() external payable { }
+    function receive() external payable {
+        lastSender == msg.sender;
 
+        // msg.sender  (senders address)
+        // msg.value  (amount sent)
+    }
 
     // Get Balance
-    function getBalance() public view returns (uint){
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
+
+
+    // Send Payment
+    function pay (address payable  addr) public  payable {
+        (bool sent, bytes memory data) = addr.call{value : 1 ether}("");
+
+        require(sent , "Error sending Eth");
+    }
+
+
+
 }
